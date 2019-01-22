f05d6a366fac3cd942397a6ac848985e91c84aab
johnfarriss <38168629+johnfarriss@users.noreply.github.com>
2019-01-22 11:30:27 -0500

    Add files via upload
    Also incomplete, just need to add link in the HTML file and should be good

---
+++ scripts/couldntthinkofaname.js
@@ -0,0 +1,13 @@
+//function to be called when login button clicked
+function userLogin() {
+    username = document.getElementById('usernameLogin').value;
+    password = document.getElementById('passwordLogin').value;
+}
+
+//function to be called when register button clicked
+function userRegister() {
+    name = document.getElementById('nameRegister').value;
+    username = document.getElementById('usernameRegister').value;
+    password = document.getElementById('passwordRegister').value;
+}
+
