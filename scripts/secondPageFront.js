7445b09ba9957e86ce0796ea4ed632a5f3d3f761
johnfarriss <38168629+johnfarriss@users.noreply.github.com>
2019-01-22 13:53:24 -0500

    Add files via upload
    Also not done yet

---
+++ scripts/secondPageFront.js
@@ -0,0 +1,32 @@
+function addContact(){
+    name = document.getElementById('addName').value;
+    number = document.getElementById('addNumber').value;
+    email = document.getElementById('addEmail').value;
+    address = document.getElementById('addAddress').value;
+}
+
+function search(){
+    name = document.getElementById('searchName').value;
+    number = document.getElementById('searchNumber').value;
+    email = document.getElementById('searchEmail').value;
+    address = document.getElementById('searchAddress').value;
+}
+
+function table() {
+    var table = document.getElementById('contactTable');
+
+    var i;
+    for(i = 0; i < numContacts; i++){
+        var row = table.insertRow(i);
+        var info = document.createElement("p");
+        var node = createTextNode(
+            "Name:" + name +"\n",
+            "Phone:" + number + "\n",
+            "E-mail:" + email + "\n",
+            "Address:" + address + "\n");
+    
+        info.appendChild(node);
+        var element = getElementById("newRow");
+        element.appendChild(info);    
+    }
+}
