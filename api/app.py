34151d9e44caabbdcf4843cfcb2900bbb298d88d
aahadley <Aaron.Hadley@knights.ucf.edu>
2019-02-05 10:50:01 -0500

    halfway working api code

--- api/app.py
+++ api/app.py
@@ -7,38 +7,118 @@ db = sql.connect( "contactm.cm0a6cdoupkr.us-east-1.rds.amazonaws.com" , "masterU
 cursor = db.cursor()
 cursor.execute("SELECT VERSION()")
 data = cursor.fetchone()
+
 print ("Database version : %s " % data)
 
-'''
-class User():
-    #id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(80), unique=True)
-    email = db.Column(db.String(120), unique=True)
 
-    def __init__(self, username, password, email):
-        self.username = username
-        self.password = password
-        self.email = email
-        '''
+# ============= USERS =============
 
-# register
-@app.route('/user', methods=['POST'])
+# register user
+@app.route('/user', methods=["POST"])
 def add_user():
 
-    username = request.json['username']
-    password = request.json['password']
-    email    = request.json['email']
+    username = request.get_json(force = True)["username"]
+    password = request.get_json(force = True)["password"]
+    email    = request.get_json(force = True)["email"   ]
 
-    #usr = User(username, password, email)
     query = "INSERT INTO contactm.user (username, password, email) VALUES (\'"+username+"\',\'"+password+"\',\'"+email+"\')"
-
-    # db.session.add(usr)
-    # db.session.commit()
-
+    
     cursor.execute(query)
-
+    db.commit()
+    db.close()
 
     return jsonify({"success": True})
+
+
+# login
+@app.route("/login", methods=["POST"])
+def login():
+
+    username = request.get_json(force = True)["username"]
+    password = request.get_json(force = True)["password"]
+
+    query = "SELECT * FROM contactm.user WHERE username = \'"+username+"\'"
+
+    cursor.execute(query)
+    return jsonify({"TODO" : "garbage"})
+
+# ============= CONTACTS ==============
+
+# add contact
+@app.route("/contactsadd", methods=["POST"])
+def add_contact():
+
+    name     = request.json["contactName" ]
+    email    = request.json["emailAddress"] #CLARIFY THIS DISTICTION
+    phone    = request.json["phoneNumber" ]
+    address  = request.json["address"     ] #CLARIFY THIS DISTICTION
+    username = request.json["username"    ]
+
+    query = "INSERT INTO contactm.contacts (contactName, emailAddress, phoneNumber, address, username) VALUES \
+        (\'"+name+"\',\'"+email+"\',\'"+phone+"\',\'"+address+"\',\'"+username+"\')"
+            
+    cursor.execute(query)
+    db.commit()
+    db.close()        
+
+    return jsonify({"success": True}, {"easteregg" : "<3 Leinecker <3"})
+
+
+
+# get contacts
+@app.route("/contactsget", methods=["POST"])
+def get_contacts():
+    
+    name     = request.json["contactName" ]
+    email    = request.json["emailAddress"] #CLARIFY THIS DISTICTION
+    phone    = request.json["phoneNumber" ]
+    address  = request.json["address"     ] #CLARIFY THIS DISTICTION
+    username = request.json["username"    ]
+
+    if not username or username == "":  #failure
+        return jsonify({"success" : False}, {"msg" : "Invalid username"})
+
+    # base SQL command
+    query = "SELECT * FROM contactm.contacts WHERE username = \'"+username+"\'"
+    
+    # Optional parameters
+    if name and name != "":
+        query += " AND contactName = \'"+name+"\'"
+        #print("\n\n" + query + "\n\n")
+    
+    if email and email != "":
+        query += " AND emailAddress = \'"+email+"\'"
+
+
+    if phone and phone != "":
+        query += " AND phoneNumber = \'"+phone+"\'"
+
+
+    if address and address != "":
+        query += " AND address = \'"+address+"\'"
+
+    
+    cursor.execute(query)
+    db.close()
+
+    return jsonify(cursor.fetchall())
+
+
+# delete contact
+@app.route("/contactsdel", methods=["POST"])
+def delete_contact():
+
+    username = request.json["username"]
+    clientID = request.json["contactID"]
+
+    query = "DELETE FROM contactm.contacts WHERE username = \'"+username+"\' AND contactID = \'"+clientID+"\'"
+    
+    cursor.execute(query)
+    db.commit()
+    db.close()
+
+    return jsonify({"success" : True})
+
 
 if __name__ == "__main__":
     app.run(debug = True)
